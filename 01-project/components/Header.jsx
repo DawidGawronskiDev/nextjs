5de56335bacb5587809a75f1bab1596b25330c82
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-05-06 18:36:20 +0100

    Nextjs routing basics

---
+++ 01-project/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+    </header>
+  );
+};
+
+export default Header;
